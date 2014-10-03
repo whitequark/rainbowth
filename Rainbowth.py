fc43e2bfef802feffbe5571d79ec8280761bfcdd
Peter Zotov <whitequark@whitequark.org>
2014-10-03 09:19:09 +0400

    ST3 compat; highlight Clojure; highlight whole file.

--- Rainbowth.py
+++ Rainbowth.py
@@ -18,7 +18,7 @@ class Rainbowth(sublime_plugin.EventListener):
       return
 
     scheme_file = open(scheme_path, 'r')
-    scheme_xml = scheme_file.read().decode('utf-8')
+    scheme_xml = scheme_file.read()#.decode('utf-8')
 
     bg = re.search('background.+?g>(.+?)<', scheme_xml, re.DOTALL).group(1)
     bg = '#%06x' % max(1, (int(bg[1:], 16) - 1))
@@ -31,45 +31,34 @@ class Rainbowth(sublime_plugin.EventListener):
 
     scheme_xml = re.sub('</array>', rainbowth + '<!---->\n\t</array>', scheme_xml)
     scheme_file = open(scheme_path, 'w')
-    scheme_file.write(scheme_xml.encode('utf-8'))
+    scheme_file.write(scheme_xml)#.encode('utf-8'))
     scheme_file.close()
     cache[scheme_name] = self.colors
-    pickle.dump(cache, open(base_dir + '/Rainbowth/Rainbowth.cache', 'w'))
+    pickle.dump(cache, open(base_dir + '/Rainbowth/Rainbowth.cache', 'wb'))
 
   def on_load(self, view):
     file_scope = view.scope_name(0)
-    self.lispy = file_scope.split('.')[1].split(' ')[0] in ['lisp', 'scheme']
-    if self.lispy:
+    view.settings().set('lispy',
+        file_scope.split('.')[1].split(' ')[0] in ['lisp', 'scheme', 'clojure'])
+    if view.settings().get('lispy'):
       self.update_colors(view)
       self.on_modified(view, True)
 
   def on_modified(self, view, load = False):
-    if not self.lispy:
-      return
-
-    key = view.substr(view.sel()[0].begin() - 1)
-    if key not in '()' and not load:
+    if not view.settings().get('lispy'):
       return
 
     source = view.substr(sublime.Region(0, view.size()))
-    src_len = len(source)
-    try:
-      block_end = source.index('\n\n(', view.sel()[0].begin())
-    except ValueError:
-      block_end = src_len
-    try:
-      block_start = block_end - source[::-1][src_len - block_end:].index('(\n\n') - 1
-    except ValueError:
-      block_start = 0
 
-    parens = [(sublime.Region(i + block_start, i + block_start + 1), c)
-      for i, c in enumerate(source[block_start:block_end]) if c in '()']
+    parens = [(sublime.Region(idx, idx + 1), char)
+      for idx, char in enumerate(source[0:view.size()])
+      if char in '()[]']
 
-    level, depths = 0, [[sublime.Region(-1, 0)] for i in xrange(len(self.colors))]
-    for p in parens:
-      level += p[1] == ')' and -1 or 0
-      depths[level % len(self.colors)].append(p[0])
-      level += p[1] == '(' and 1 or 0
+    level, depths = 0, [[sublime.Region(-1, 0)] for i in range(len(self.colors))]
+    for region, char in parens:
+      level += (char in '([') and -1 or 0
+      depths[level % len(self.colors)].append(region)
+      level += (char in ')]') and 1 or 0
 
     for i, regions in enumerate(depths):
       view.erase_regions('rainbowth%d' % i)
